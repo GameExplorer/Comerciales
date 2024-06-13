ca0c2b3a575d2960068c317f5d29fa2921645ef9
Petar <petar.petrovic2001@gmail.com>
2024-06-13 11:22:58 +0100

    Update controlPanel.php

--- controlPanel.php
+++ controlPanel.php
@@ -55,7 +55,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Control Panel</title>
+    <title>Panel de Control</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
 </head>

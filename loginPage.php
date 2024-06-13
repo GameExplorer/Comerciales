fd47cc0818e7b90c97e7806e316713abbdb1bb6c
GameExplorer <petar.petrovic2001@gmail.com>
2024-06-13 11:12:28 +0100

    Update loginPage.php

--- loginPage.php
+++ loginPage.php
@@ -45,6 +45,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Login</title>
         <link rel="stylesheet" href="Login.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://getbootstrap.com/docs/5.3/assets/css/docs.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
         <style>
             #brrr {
                 margin-top: 100px;
